a71c248d0b3ae4642c33506f29c71a5b330ac0b5
likhitha-29 <127423840+likhitha-29@users.noreply.github.com>
2023-05-07 22:24:07 +0530

    Update Query1.py

--- Query1.py
+++ Query1.py
@@ -1,6 +1,6 @@
 import mysql.connector
 mydb=mysql.connector.connect(host='localhost',user='root',password='Likhitha@29',database="Inventory_Control_Management")
 c=mydb.cursor()
-f='delete from Manufacture1 where defective_items>=1'
+f='delete from Manufacture1,Purchase1 from Purchase1 p JOIN Manufacture1 m on m.product=p.product where defective_items>=1'
 c.execute(f)
 mydb.commit()
